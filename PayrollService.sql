5b1a7f7af3cbb59dc4d88aba1ce6798cc0b4b50b
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-29 01:29:22 +0530

    [abhineet] add : ability to add records into a table

--- PayrollService.sql
+++ PayrollService.sql
@@ -27,3 +27,13 @@ drop table employee_payroll;
 
 # Display details of fields of table
 describe employee_payroll;
+
+# Insert employee records into the table
+insert into employee_payroll 
+(name, salary, start)
+values
+   ('Bill', 1000000.00, '2018-01-03'),
+   ('Mark', 2000000.00, '2019-11-13'),
+   ('Charlie', 3000000.00, '2020-05-21');
+
+
