1d745bfa5a0f75162b6a28623956218a6ff4f349
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-29 01:20:12 +0530

    [abhineet] add : ability to show, add and use database

--- PayrollService.sql
+++ PayrollService.sql
@@ -1,1 +1,15 @@
 #PayrollService
+
+# To see existing databases
+show databases;
+
+# Create a new database
+create database payroll_service;
+
+# To use a particular database
+use payroll_service;
+
+# To see currently used database
+select database();
+
+
